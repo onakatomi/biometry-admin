2fb79a539959150a1dcedc2b88ca53b9e62ef1c5
Ollie Quarm <ollie@nakatomi.com>
2025-07-15 14:08:19 +1000

    Initial Commit

---
+++ Biometry/BiometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometryApp.swift
+//  Biometry
+//
+//  Created by Nakatomi on 15/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct BiometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Biometry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Biometry
+//
+//  Created by Nakatomi on 15/7/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
