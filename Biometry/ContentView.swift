d7c9fbdcfa7a46053427d6e89044d5eb091639b9
Ollie Quarm <ollie@nakatomi.com>
2025-07-17 11:28:51 +1000

    Fix memory issue, add comments

--- Biometry/ContentView.swift
+++ Biometry/ContentView.swift
@@ -12,18 +12,18 @@ struct ContentView: View {
     
     @State private var videoUrls: [URL] = [] // Videos selected.
     @State private var externalScreenVideoPlayers: [AVPlayer] = [] // Active list of players (each corresponding to a video)
-    @State private var previewVideoPlayers: [AVPlayer] = []
-    @State private var screenWindows: [NSWindow] = [] // Keep track of screens in use.
+    @State private var previewVideoPlayers: [AVPlayer] = [] // Video players for inline preview
+    @State private var screenWindows: [NSWindow] = [] // Keep track of screens currently in playback
     @State private var areVideosPlaying = false // Whether we're currently in playback
     @State private var selectedVideoForScreen: [Int?] = [] // one selected-video index per screen; default to 0
-    @StateObject private var model = DisplayModel()
+    @StateObject private var model = DisplayModel() // Model giving us screen info
     
     var body: some View {
         ZStack {
             // Admin panel background colour
             Color(.darkGray)
                 .ignoresSafeArea()
-
+            
             VStack(spacing: 16) {
                 Image(systemName: "toilet")
                     .font(.system(size: 40, weight: .regular))
@@ -36,12 +36,12 @@ struct ContentView: View {
                 if !videoUrls.isEmpty {
                     HStack {
                         ForEach(previewVideoPlayers.indices, id: \.self) { i in
-                          let player = previewVideoPlayers[i]
-                          VideoPlayer(player: player)
-                            .frame(height: 100)
-                            .aspectRatio(contentMode: .fit)
-                            .clipped()
-                            .onAppear { player.play() }
+                            let player = previewVideoPlayers[i]
+                            VideoPlayer(player: player)
+                                .frame(height: 100)
+                                .aspectRatio(contentMode: .fit)
+                                .clipped()
+                                .onAppear { player.play() }
                         }
                     }
                 } else { Text("Pick some videos to preview...") }
@@ -83,37 +83,33 @@ struct ContentView: View {
                 .padding(.top)
                 
                 // If we're not currently playing a video and we have at least 1 video to show.
-                if !areVideosPlaying && externalScreenVideoPlayers.count >= 1 {
+                if (!areVideosPlaying) {
                     PrimaryButton(text: "Start experience!") {
                         areVideosPlaying = true
                         launchOnScreens()
                     }
-                } else if areVideosPlaying {
-                    PrimaryButton(text: "Stop playback") {
-                        stopPlayback()
-                        print("stopped playback")
-                    }
+                } else {
+                    PrimaryButton(text: "Stop playback") { stopPlayback() }
                 }
             }
-            .padding()
-        }
-        // Rebuild player array whenever the user picks new files (which triggers videoUrls to update)
-        .onChange(of: videoUrls) { _, newUrls in
-            externalScreenVideoPlayers = newUrls.map { AVPlayer(url: $0) }
-            previewVideoPlayers = newUrls.map { AVPlayer(url: $0) }
-            
-            // Reset selections when the videos change
-            selectedVideoForScreen = Array(
-                repeating: nil,
-                count: model.screens.count
-            )
-        }
-        // Keep list of screen-to-video mappings in-sync when screens change
-        .onChange(of: model.screens) { _, screens in
-            selectedVideoForScreen = Array(
-                repeating: nil,
-                count: screens.count
-            )
+            // Rebuild player array whenever the user picks new files (which triggers videoUrls to update)
+            .onChange(of: videoUrls) { _, newUrls in
+                externalScreenVideoPlayers = newUrls.map { AVPlayer(url: $0) }
+                previewVideoPlayers = newUrls.map { AVPlayer(url: $0) }
+                
+                // Reset selections when the videos change
+                selectedVideoForScreen = Array(
+                    repeating: nil,
+                    count: model.screens.count
+                )
+            }
+            // Keep list of screen-to-video mappings in-sync when screens change
+            .onChange(of: model.screens) { _, screens in
+                selectedVideoForScreen = Array(
+                    repeating: nil,
+                    count: screens.count
+                )
+            }
         }
     }
     
@@ -147,31 +143,27 @@ struct ContentView: View {
             // Set AV settings
             let avView = AVPlayerView(frame: screen.frame)
             avView.player = externalScreenVideoPlayers[videoIndex!]
-            avView.player?.play()
-            avView.controlsStyle = .none
+            avView.player?.seek(to: .zero) // Play the videos from the start
+            avView.player?.play() // Start the video immediately
+            avView.controlsStyle = .none // Hide controls
             avView.videoGravity = .resizeAspect
             
             // Set window settings
             window.setFrame(screen.frame, display: true) // Center on screen and size to fill
-            window.level = .screenSaver
+            window.level = .screenSaver // Full screen
             window.contentView = avView // Place the AV View into the screen
-            window.makeKeyAndOrderFront(nil)
-            window.toggleFullScreen(nil)
-            window.isReleasedWhenClosed = true
-            screenWindows.append(window) // Add this window to list of currently tracked windows
+            window.makeKeyAndOrderFront(nil) // Bring to front
+            window.isReleasedWhenClosed = false // Do not immediately deallocate the window object as soon as it’s closed -- gives us full control over its lifeitme
+            
+            screenWindows.append(window)  // Add this window to list of currently tracked windows
         }
-        
-        print("launched windows")
     }
     
     // Stop playback on all screens
     private func stopPlayback() {
-      screenWindows.forEach { $0.close() }
-      screenWindows.removeAll()
-        
-        print("removed windows")
-        
-      areVideosPlaying = false
+        screenWindows.forEach { $0.close() } // Close each window
+        screenWindows.removeAll() // Remove all screens from active screens array
+        areVideosPlaying = false
     }
     
 }
